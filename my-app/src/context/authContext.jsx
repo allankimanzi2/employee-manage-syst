e959b5990a30c7dc720ce11e63acfd92c9579812
allankimanzi2 <allankim457@gmail.com>
2025-04-06 21:41:43 +0300

    Login Functionality & Authentication Setup

---
+++ my-app/src/context/authContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios'
+
+
+
+
+const userContext = createContext()
+
+const authContext = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() =>{
+      const verifyUser = async () => {
+        try {
+          const token = localStorage.getItem('token')
+          if(token) {
+            const response = await axios.get('http://localhost:5000/apo/auth/verify', {
+              headers: {
+                "Authorization" : `Bearer ${token}`
+              }
+            })
+            if(response.data.success) {
+              setUser(response.data.user)
+            }
+        } else {
+            setUser(null)
+        }
+        } catch (error) {
+          if(error.response && !error.response.data.error) {
+              setUser(null)
+          }
+
+        } finally {
+            setLoading(false)
+        }
+      }
+      verifyUser()
+    }, [])
+
+    const login = (user) => {
+        setUser(user)
+
+    }
+
+    const logout = () => {
+        setUser(null)
+        localStorage.remove.Item("token")
+    }
+  return (
+    <userContext.Provider value={{user, login, logout, loading}}>
+        {children}
+
+    </userContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(userContext)
+export default authContext
